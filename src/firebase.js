0c505e3b08284cfb6908e43deca786f75e399e7f
VishalAle <126844340+VishalAle@users.noreply.github.com>
2026-01-22 12:23:17 +0530

    credentialsfix

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMW9soE-m1RXbqE8mRoMp40kv3wCUK5X0",
-  authDomain: "netflix-clone-33fdc.firebaseapp.com",
-  projectId: "netflix-clone-33fdc",
-  storageBucket: "netflix-clone-33fdc.firebasestorage.app",
-  messagingSenderId: "945463915851",
-  appId: "1:945463915851:web:ab42013b1d9c66ae35a4ce",
-  measurementId: "G-HC7NC60FR9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
